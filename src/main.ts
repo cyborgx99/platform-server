ee2d65b3a309751ea5acf4ba8ce81225ef7b0004
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-03-09 20:51:00 +0200

    Add validation pipe exception factory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 
@@ -6,7 +10,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        const error_messages = errors.map((error) =>
+          Object.values(error.constraints),
+        );
+        return new BadRequestException(error_messages.toString());
+      },
+    }),
+  );
   app.use(cookieParser());
   await app.listen(3001);
 }
