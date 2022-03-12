25d7952ddc7bda98cba17b13996fdf7d51ca0239
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-03-12 22:33:12 +0200

    Add mail service spec

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { MailModule } from './mail.module';
 import { MailService } from './mail.service';
 
 describe('MailService', () => {
@@ -8,12 +9,24 @@ describe('MailService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MailService],
+      imports: [MailModule],
     }).compile();
+    const app = module.createNestApplication();
+    await app.init();
 
     service = module.get<MailService>(MailService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should send an email', () => {
+    service.sendEmail(
+      'cyborgx999@gmail.com',
+      'Hello',
+      { name: 'sergey', url: 'ffs' },
+      'confirmation',
+    );
   });
 });
