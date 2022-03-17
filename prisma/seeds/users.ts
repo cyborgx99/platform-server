8d9d5600f986ba168461d6fee221ecd0239ce59a
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-03-17 17:43:52 +0200

    Add seeds

---
+++ prisma/seeds/users.ts
@@ -0,0 +1,10 @@
+import { Prisma } from '@prisma/client';
+
+export const users: Prisma.UserCreateInput[] = [
+  {
+    name: 'Bong',
+    lastName: 'Johnson',
+    email: 'cyborgx999@gmail.com',
+    password: '123qwe',
+  },
+];
