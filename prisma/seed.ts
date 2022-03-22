1d945566faa1749e63065589051934601c0a08c7
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-03-22 22:02:37 +0200

    Add more seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 import { users } from './seeds/users';
 const prisma = new PrismaClient();
 
 async function main() {
+  const salt = await bcrypt.genSalt();
+  const usersWithHashedPassword = await Promise.all(
+    users.map(async (user) => {
+      const hashed = await bcrypt.hash(user.password, salt);
+      return { ...user, password: hashed };
+    }),
+  );
+
   await prisma.user.createMany({
-    data: users,
+    data: usersWithHashedPassword,
   });
 }
 
--- prisma/seeds/users.ts
+++ prisma/seeds/users.ts
@@ -2,15 +2,88 @@ import { Prisma } from '@prisma/client';
 
 export const users: Prisma.UserCreateInput[] = [
   {
-    name: 'Bong',
+    name: 'Boris',
     lastName: 'Johnson',
     email: 'cyborgx999@gmail.com',
     password: '123qwe',
+    role: 'TEACHER',
   },
   {
-    name: 'Bong',
+    name: 'Dwayne',
     lastName: 'Johnson',
-    email: 'cyborgx9991@gmail.com',
+    email: 'cyborgx999+1@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Big',
+    lastName: 'Ben',
+    email: 'cyborgx999+2@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Little',
+    lastName: 'Ben',
+    email: 'cyborgx999+3@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Tiny',
+    lastName: 'Ben',
+    email: 'cyborgx999+4@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'What',
+    lastName: 'Who',
+    email: 'cyborgx999+5@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Lao',
+    lastName: 'Tzu',
+    email: 'cyborgx999+6@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Xi',
+    lastName: 'Jinping',
+    email: 'cyborgx999+7@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Volodymyr',
+    lastName: 'Zelensky',
+    email: 'cyborgx999+8@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Thomas',
+    lastName: 'Anderson',
+    email: 'cyborgx999+9@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Ding',
+    lastName: 'Liren',
+    email: 'cyborgx999+10@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Agent',
+    lastName: 'Smith',
+    email: 'cyborgx999+11@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Carrie-Anne',
+    lastName: 'Moss',
+    email: 'cyborgx999+12@gmail.com',
+    password: '123qwe',
+  },
+  {
+    name: 'Kurt',
+    lastName: 'Cobain',
+    email: 'cyborgx999+13@gmail.com',
     password: '123qwe',
   },
 ];
