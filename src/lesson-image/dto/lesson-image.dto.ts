a4e9ab2d96a72ddd25402bb669df08d8ca77a779
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-03-29 22:40:36 +0300

    Add update and delete mutations and methods

--- src/lesson-image/dto/lesson-image.dto.ts
+++ src/lesson-image/dto/lesson-image.dto.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PartialType } from '@nestjs/graphql';
 
 import { LessonImage } from '../models/lesson-image.model';
 
@@ -14,6 +14,22 @@ export class CreateLessonImageInput {
   publicId?: string;
 }
 
+@InputType()
+export class DeleteLessonImageInput {
+  @Field()
+  id: number;
+
+  @Field(() => String, { nullable: true })
+  publicId?: string;
+}
+
+@InputType()
+export class UpdateLessonImageInput extends PartialType(
+  CreateLessonImageInput,
+) {
+  @Field()
+  id: number;
+}
 @ObjectType()
 export class GetLessonImagesResponse {
   @Field(() => [LessonImage], { nullable: 'items' })
--- src/lesson-image/lesson-image.module.ts
+++ src/lesson-image/lesson-image.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 import { PrismaModule } from 'src/database/prisma.module';
 
 import { LessonImageResolver } from './lesson-image.resolver';
@@ -6,6 +7,6 @@ import { LessonImageService } from './lesson-image.service';
 
 @Module({
   providers: [LessonImageResolver, LessonImageService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, CloudinaryModule],
 })
 export class LessonImageModule {}
